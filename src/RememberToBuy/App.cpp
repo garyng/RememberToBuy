d26379766930e12801d29aa10c6bd2e5660712bd
GaryNg <garyng.zhongbo@gmail.com>
2018-08-12 19:10:48 +0800

    refactor: move imgui boilerplate to App class

---
+++ src/RememberToBuy/App.cpp
@@ -0,0 +1,126 @@
+﻿#include "stdafx.h"
+#include "App.h"
+
+
+using namespace std;
+
+void App::Initialize()
+{
+	glfwSetErrorCallback([](int error, const char* description)
+	{
+		std::cout << "Error " << error << ": " << description << std::endl;
+	});
+
+	glfwInit();
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	_window = glfwCreateWindow(1280, 720, WINDOW_TITLE.c_str(), nullptr, nullptr);
+
+	glfwMakeContextCurrent(_window);
+	glfwSwapInterval(1); // Enable vsync
+	gl3wInit();
+
+	// Setup Dear ImGui binding
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	(void)io;
+
+	ImGui_ImplGlfw_InitForOpenGL(_window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	ImGui::StyleColorsDark();
+}
+
+void App::Loop()
+{
+	ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	while (!glfwWindowShouldClose(_window))
+	{
+		glfwPollEvents();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		Render();
+
+		ImGui::Render();
+
+		int display_w, display_h;
+		glfwMakeContextCurrent(_window);
+		glfwGetFramebufferSize(_window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwMakeContextCurrent(_window);
+		glfwSwapBuffers(_window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(_window);
+	glfwTerminate();
+}
+
+void App::Render() const
+{
+	static bool showDemoWindow = true;
+	static bool showAnotherWindow = false;
+
+
+	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+	if (showDemoWindow)
+		ImGui::ShowDemoWindow(&showDemoWindow);
+
+	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+	{
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text."); // Display some text (you can use a format strings too)
+		ImGui::Checkbox("Demo Window", &showDemoWindow); // Edit bools storing our window open/close state
+		ImGui::Checkbox("Another Window", &showAnotherWindow);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f); // Edit 1 float using a slider from 0.0f to 1.0f    
+		// ImGui::ColorEdit3("clear color", (float*)&clearColor); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))
+			// Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+		            ImGui::GetIO().Framerate);
+		ImGui::End();
+	}
+
+	// 3. Show another simple window.
+	if (showAnotherWindow)
+	{
+		ImGui::Begin("Another Window", &showAnotherWindow);
+		// Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Me"))
+			showAnotherWindow = false;
+		ImGui::End();
+	}
+
+
+}
+
+void App::Start()
+{
+	Initialize();
+	Loop();
+}
---
+++ src/RememberToBuy/App.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl3.h>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
+class App
+{
+private:
+	GLFWwindow* _window;
+	const std::string WINDOW_TITLE = "Remember To Buy";
+	void Initialize();
+	void Loop();
+	void Render() const;
+
+public:
+	void Start();
+};
