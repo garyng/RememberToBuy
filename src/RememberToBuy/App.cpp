928efbcbc4c3ed0bb5c84171eb0bc53ca0f37d34
GaryNg <garyng.zhongbo@gmail.com>
2018-08-13 09:21:04 +0800

    lib: add boost/di

--- src/RememberToBuy/App.cpp
+++ src/RememberToBuy/App.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void App::Initialize()
 {
+	
 	glfwSetErrorCallback([](int error, const char* description)
 	{
 		std::cout << "Error " << error << ": " << description << std::endl;
