020e79ddcd1f85ce59bee764c2981b2cd52c1ea1
GaryNg <garyng.zhongbo@gmail.com>
2018-09-22 21:29:17 +0800

    feat: add sorting utils for mapping sort key to order_by lambda

---
+++ src/ui/utils/Sorter.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include "Sorter.h"
+#include <coveo/linq.h>
+
+void Sorter::Sort(std::vector<CartItem>& items, CartItemFields field, bool ascending)
+{
+	using namespace coveo::linq;
+
+	OrderBy(items, ascending, [](CartItem item) { return item.Item().Value().Name(); });
+	switch (field)
+	{
+		case CartItemFields::Id:
+			OrderBy(items, ascending, [](CartItem item) { return item.Id(); });
+			break;
+		case CartItemFields::Quantity:
+			OrderBy(items, ascending, [](CartItem item) { return item.Quantity(); });
+			break;
+		case CartItemFields::Name:
+			OrderBy(items, ascending, [](CartItem item) { return item.Item().Value().Name(); });
+			break;
+	}
+}
